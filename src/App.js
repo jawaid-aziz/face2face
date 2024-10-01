d02b78fed1ced9782d6e84bb3946995ba861aaa1
Jawaid Aziz <javaidmemon24@gmail.com>
2024-10-01 21:35:23 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Lobby from './screens/Lobby.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Lobby/>}/>
+      </Routes>
     </div>
   );
 }
