c31b653cd588ea0d0a6962da15d2e914de802da0
Jawaid Aziz <javaidmemon24@gmail.com>
2024-10-01 21:37:50 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Lobby from './screens/Lobby.jsx';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Lobby/>}/>
+        <Route path='/' element={}/>
       </Routes>
     </div>
   );
