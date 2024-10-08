4003350d95905e791e28d2dd7b976eac71153513
Jawaid Aziz <javaidmemon24@gmail.com>
2024-10-08 12:43:47 +0500

    commit

--- src/screens/Room.jsx
+++ src/screens/Room.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import ReactPlayer from 'react-player';
-import { useSocket } from '../context/SocketProvider'
+import { useSocket } from '../context/SocketProvider';
+import peer from "../service/peer";
 
 export const RoomPage = () => {
 
@@ -19,17 +20,27 @@ export const RoomPage = () => {
           audio: true,
           video: true,
         });
+        const offer = await peer.getOffer();
+        socket.emit("user: call", {to: remoteSocketId, offer});
         setMyStream(stream);
       } catch (error) {
         console.error('Error accessing media devices.', error);
       }
-    }, []);
+    }, [remoteSocketId, socket]);
     
+    const handleIncomingCall = useCallback( ({from,offer}) => {
+      console.log(`Incoming Call`, from, offer);
+    }, []);    
 
     useEffect( () => {
         socket.on('user:joined', handleUserJoined);
-        return () => { socket.off('user:joined', handleUserJoined) };
-    },[socket, handleUserJoined]);
+        socket.on("incoming:call", handleIncomingCall);
+
+        return () => { 
+        socket.off('user:joined', handleUserJoined);
+        socket.off("incoming:call", handleIncomingCall);
+      };
+    },  [socket, handleUserJoined, handleIncomingCall]);
 
   return (
     <div>
@@ -50,6 +61,5 @@ export const RoomPage = () => {
           </>
         )}
     </div>
-
   )
 }
